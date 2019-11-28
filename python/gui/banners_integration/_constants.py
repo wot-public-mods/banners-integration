7933d482e0709af3d3268ba184bc5f83e7cc27e5
Andrey Andruschyshyn <p0lir0id@yandex.ru>
2019-11-29 00:11:15 +0200

    fixed problems with working directory

--- python/gui/banners_integration/_constants.py
+++ python/gui/banners_integration/_constants.py
@@ -1,11 +1,14 @@
 
 import os
+import sys
 
 __all__ = ('IS_DEV', 'ENTITIES_PATH', 'MODEL', 'MODEL_PATH')
 
-IS_DEV = os.path.isfile('../banners-integration.dev')
+# Client installation folder (same to './../' but does not depend on the working directory)
+CLIENT_ROOT = os.path.sep.join(sys.executable.split(os.path.sep)[:-2])
+IS_DEV = os.path.isfile('%s/banners-integration.dev' % CLIENT_ROOT)
 
-ENTITIES_PATH = '../mods/resources/banners.integration'
+ENTITIES_PATH = '%s/mods/resources/banners.integration' % CLIENT_ROOT
 
 MODEL_PATH = 'content/mods/banners.integration/%s.model'
 
