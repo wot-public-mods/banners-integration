3b857027f9c76820b5b9f181faea716cda4afea6
Andrey Andruschyshyn <poliroid@pm.me>
2022-08-07 23:01:34 +0300

    credentials

--- python/gui/banners_integration/__init__.py
+++ python/gui/banners_integration/__init__.py
@@ -1,10 +1,10 @@
-__author__ = "Andruschyshyn Andrey"
-__copyright__ = "Copyright 2021, poliroid"
-__credits__ = ["Andruschyshyn Andrey"]
+__author__ = "Andrii Andrushchyshyn"
+__copyright__ = "Copyright 2022, poliroid"
+__credits__ = ["Andrii Andrushchyshyn"]
 __license__ = "LGPL-3.0-or-later"
 __version__ = "1.2.8"
-__maintainer__ = "Andruschyshyn Andrey"
-__email__ = "poliroid@pm.me"
+__maintainer__ = "Andrii Andrushchyshyn"
+__email__ = "contact@poliroid.me"
 __status__ = "Production"
 
 from gui.banners_integration.controller import g_instance
